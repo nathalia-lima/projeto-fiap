095340fd52a67a85810686a5a0e8f795bbcf2be5
LucasSilvaCaitano <lucas.caitano@bandtec.com.br>
2025-08-10 19:16:36 -0300

    Add fields validation

--- restaurante/src/main/java/com/fiap/java/restaurante/exceptions/handler/GlobalExceptionHandler.java
+++ restaurante/src/main/java/com/fiap/java/restaurante/exceptions/handler/GlobalExceptionHandler.java
@@ -1,18 +1,19 @@
-package com.fiap.java.restaurante.exceptions;
+package com.fiap.java.restaurante.exceptions.handler;
 
-import org.springframework.http.HttpStatus;
+import com.fiap.java.restaurante.DTO.ValidacaoRespostasDto;
+import com.fiap.java.restaurante.exceptions.BadRequestException;
+import com.fiap.java.restaurante.exceptions.NotFoundException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import com.fiap.java.restaurante.exceptions.NotFoundException;
-import com.fiap.java.restaurante.exceptions.BadRequestException;
 import com.fiap.java.restaurante.DTO.RespostaDTO;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.List;
+
 @RestControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<RespostaDTO> handleNotFoundException(NotFoundException ex, WebRequest request) {
@@ -22,6 +23,14 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<RespostaDTO> handleBadRequestException(BadRequestException ex, WebRequest request) {
         return ResponseEntity.status(400).body(new RespostaDTO ("400", ex.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidacaoRespostasDto> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, WebRequest request) {
+        List<String> erros = ex.getBindingResult().getFieldErrors().stream().map(fieldError ->
+                String.format("%s : %s", fieldError.getField(), fieldError.getDefaultMessage())).toList();
+
+        return ResponseEntity.status(400).body(new ValidacaoRespostasDto(erros, 400));
     }
 
 }
